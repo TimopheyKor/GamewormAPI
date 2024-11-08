dc7b796fa36c391facbe7fb143af2e4ba7b7df93
TimopheyKor <timopheykor@gmail.com>
2024-11-08 13:25:05 -0500

    removed gin, prep for google docs requests

--- main.go
+++ main.go
@@ -3,12 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
-	"net/http"
-
-	// Maybe don't use gin - Damie Suggestion
-	"github.com/gin-gonic/gin"
 )
 
+// TODO: Create functionalityh to send requests to Google Docs
 func main() {
 	lvlFlag := flag.String("l", "info", "description of log level")
 
@@ -17,12 +14,4 @@ func main() {
 	fmt.Printf("level is %s\n", *lvlFlag)
 	fmt.Println("Gameworm API Blank Commit")
 	fmt.Println("Hello, World!")
-
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
