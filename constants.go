f8fb3a1e506b8d315921857dafc86087b2c98dd8
TimopheyKor <timopheykor@gmail.com>
2024-11-11 18:32:57 -0500

    cleaning update functionality

--- constants.go
+++ constants.go
@@ -6,6 +6,7 @@ import (
 
 // Define global constants:
 // TODO: Rename unexported constants to prefix with _
+// TODO: Replace Schema constants with a Schema file
 const (
 	// Google Sheet Temp Schema:
 	dbSpreadsheetName = "GamewormDataDoNotEdit"
@@ -18,6 +19,15 @@ const (
 	backlogRange      = "A1:E"
 	// TODO: Move this path to a more secure location when hosting.
 	clientConfigPath = "C:/data/temp_credentials/credentials.json"
+)
+
+// Define column names:
+// TODO: Replace Schema vars with a Schema file
+var (
+	gameCols = [][]any{{"Unique Game ID", "Game Title", "Game Image", "Game Developer",
+		"Game Publisher"}}
+	reviewCols  = [][]any{{"Unique Game ID", "Steam Review", "Steam Rating", "User Review", "User Rating"}}
+	backlogCols = [][]any{{"Game ID", "Priority", "Steam Flag"}}
 )
 
 // Define global errors:
--- sheethelpers.go
+++ sheethelpers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"google.golang.org/api/sheets/v4"
 )
@@ -10,8 +11,10 @@ import (
 // creating the DB.
 
 // Using a context and a sheets service, creates a new spreadsheet to hold
-// the Gameworm DB, then returns the spreadsheet's SheetID.
+// the Gameworm DB, initializes the schema of the DB, then returns the
+// spreadsheet's SheetID.
 func newSheetDB(ctx context.Context, srv *sheets.Service) (string, error) {
+	// Create a new spreadsheet with three sheets:
 	spreadsheet, err := srv.Spreadsheets.Create(&sheets.Spreadsheet{
 		Properties: &sheets.SpreadsheetProperties{
 			Title: dbSpreadsheetName,
@@ -37,19 +40,30 @@ func newSheetDB(ctx context.Context, srv *sheets.Service) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return spreadsheet.SpreadsheetId, nil
+	// Initialize the header values (column names) for the three sheets:
+	spreadsheetId := spreadsheet.SpreadsheetId
+	updateFn := prepUpdateCall(ctx, srv, spreadsheetId)
+	 if _, err = updateFn(gameD+"!A1:E1", gameCols) != nil { return "", err }
+	return spreadsheetId, nil
 }
 
 func prepUpdateCall(ctx context.Context, srv *sheets.Service, sheetId string) func(string, [][]any) (string, error) {
-	return func(s string, a [][]any) (string, error) {
-		return srv.Spreadsheets
+	return func(sRange string, values [][]any) (string, error) {
+		res, err := srv.Spreadsheets.Values.Update(sheetId, sRange, &sheets.ValueRange{
+			MajorDimension: "ROWS",
+			Values:         values,
+		}).ValueInputOption("RAW").Context(ctx).Do()
+		if err != nil {
+			return "", err
+		}
+		return fmt.Sprintf("update response: %+v\n", res), err
 	}
 }
 
-func updateRowCells(sRange string, values [][]any) (string, error) {
-	res, err := srv.Spreadsheets
-}
+// func updateRowCells(sRange string, values [][]any) (string, error) {
+// 	res, err := srv.Spreadsheets
+// }
 
-updateFn := prepSheetsCall(ctx, srv, id)
-s, err := updateFn(range, vals)
-s, err := updateFn(range2, vals2)
+// updateFn := prepSheetsCall(ctx, srv, id)
+// s, err := updateFn(range, vals)
+// s, err := updateFn(range2, vals2)
