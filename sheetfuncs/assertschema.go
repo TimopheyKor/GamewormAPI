b213fdb1279d7f69ca80b017b0e807c18f7a5af1
TimopheyKor <timopheykor@gmail.com>
2024-11-12 16:29:19 -0500

    adding assert functions to sheetfuncs package

---
+++ sheetfuncs/assertschema.go
@@ -0,0 +1,16 @@
+package sheetfuncs
+
+// TODO: Write a function that generates a UNIQUE GAME ID for a game.
+// This should maybe go in a different package.
+func NewGame() {}
+
+// TODO: Write a function that asserts that a game update or append call comes
+// with a game ID, creating an ID if one is not provided.
+func CheckGameID() {}
+
+// TODO: Implement GameIdExists
+// GameIdExists takes a SheetsHelper object and a GameID, returning True if
+// it already exists in the database, false otherwise.
+func GameIdExists(gameId string) bool {
+	return false
+}
